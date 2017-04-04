efb58eef9abd5c30a1ff4067816b53bf518d08c4
Unknown <zulberg@mail.csuchico.edu>
2017-04-04 11:48:09 -0700

    Lab complete

--- groceries.js
+++ groceries.js
@@ -1,3 +1,12 @@
+function addItem() {
+  var input=document.getElementById("newItem").value;
+  var list=document.getElementById("listDisplay");
+  var item=document.createElement("li");
+  var itemName=document.createTextNode(input);
+  item.appendChild(itemName);
+  list.appendChild(item);
+  document.getElementById("newItem").value="";
+}
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
